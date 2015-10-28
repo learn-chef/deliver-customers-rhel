b25c10d04962a86771d1b275e6773d0ad7a56265
tpetchel <tpetchel@gmail.com>
2015-10-28 09:43:43 -0400

    provision the environments

--- .delivery/build-cookbook/recipes/_aws_creds.rb
+++ .delivery/build-cookbook/recipes/_aws_creds.rb
@@ -3,37 +3,36 @@
 # Recipe:: _aws_creds
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-# Switch from using an in-memory Chef server to Chef Delivery's Chef server.
-Chef_Delivery::ClientHelper.enter_client_mode_as_delivery
+with_server_config do
+  # Read common configuration options from node attributes so that we can later access them more easily.
+  project = node['delivery']['change']['project'] # for example, 'deliver-customers-rhel'
+  stage = node['delivery']['change']['stage'] # for example, 'acceptance' or 'union'
+  driver = node[project][stage]['driver'] # for example, 'aws' or 'ssh'
+  region = node[project][stage][driver]['config']['region'] # for example, 'us-west-2'
+  profile = node[project][stage][driver]['config']['profile'] # for example, 'default'
 
-# Read common configuration options from node attributes so that we can later access them more easily.
-project = node['delivery']['change']['project'] # for example, 'deliver-customers-rhel'
-stage = node['delivery']['change']['stage'] # for example, 'acceptance' or 'union'
-driver = node[project][stage]['driver'] # for example, 'aws' or 'ssh'
-region = node[project][stage][driver]['config']['region'] # for example, 'us-west-2'
-profile = node[project][stage][driver]['config']['profile'] # for example, 'default'
+  # Decrypt the AWS credentials from the data bag.
+  aws_creds = encrypted_data_bag_item_for_environment('provisioning-data', 'aws_creds')
 
-# Decrypt the AWS credentials from the data bag.
-aws_creds = encrypted_data_bag_item_for_environment('provisioning-data', 'aws_creds')
+  # Create a string to hold the contents of the credentials file.
+  aws_config_contents = <<-EOF
+  [#{profile}]
+  region = #{region}
+  aws_access_key_id = #{aws_creds['access_key_id']}
+  aws_secret_access_key = #{aws_creds['secret_access_key']}
+  EOF
 
-# Create a string to hold the contents of the credentials file.
-aws_config_contents = <<EOF
-[#{profile}]
-region = #{region}
-aws_access_key_id = #{aws_creds['access_key_id']}
-aws_secret_access_key = #{aws_creds['secret_access_key']}
-EOF
+  # Compute the path to the credentials file.
+  aws_config_filename = File.join(node['delivery']['workspace']['cache'], node['delivery']['change']['project'], 'aws_config')
 
-# Compute the path to the credentials file.
-aws_config_filename = File.join(node['delivery']['workspace']['cache'], node['delivery']['change']['project'], 'aws_config')
+  # Write the AWS credentials to disk.
+  # Alternatively, you can use the template resource.
+  file aws_config_filename do
+    sensitive true
+    content aws_config_contents
+  end
 
-# Write the AWS credentials to disk.
-# Alternatively, you can use the template resource.
-file aws_config_filename do
-  sensitive true
-  content aws_config_contents
+  # Set the AWS_CONFIG_FILE environment variable.
+  # Chef provisioning reads this environment variable to access the AWS credentials file.
+  ENV['AWS_CONFIG_FILE'] = aws_config_filename
 end
-
-# Set the AWS_CONFIG_FILE environment variable.
-# Chef provisioning reads this environment variable to access the AWS credentials file.
-ENV['AWS_CONFIG_FILE'] = aws_config_filename
